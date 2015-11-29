08464ef0bbe92a32dd5c6c486e0c89775368b294
Ivan Krechetov <ikr@ikr.su>
2015-11-29 10:15:09 +0100

    Don't refer to google.maps before it's loaded

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-/* global global */
-const maps = global.window.google.maps;
-
 export default React.createClass({
     render() {
         return <div className='hotels-on-map'></div>;
     },
 
     componentDidMount() {
+        /* global global */
+        const maps = global.window.google.maps;
+
         new maps.Map(this.getDOMNode(), {
             zoom: 5,
             center: {lat: -34.397, lng: 150.644}
