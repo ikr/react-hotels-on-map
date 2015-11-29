ecf31ededeb22aecfcbb98e69d56526d72b891f6
Ivan Krechetov <ikr@ikr.su>
2015-11-29 10:04:08 +0100

    Make the map appear

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+/* global global */
+const maps = global.window.google.maps;
+
 export default React.createClass({
     render() {
-        return <div/>;
+        return <div className='hotels-on-map'></div>;
+    },
+
+    componentDidMount() {
+        new maps.Map(this.getDOMNode(), {
+            zoom: 5,
+            center: {lat: -34.397, lng: 150.644}
+        });
     }
 });
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,11 +1,27 @@
 import assert from 'assert';
 import React from 'react/addons';
+import mapify from './helpers/mapify';
+import massert from './helpers/massert';
 import HotelsOnMap from '../src/index';
 
 const tu = React.addons.TestUtils;
 
 describe('HotelsOnMap -- the public API', () => {
+    before(mapify);
+
     it('is a React component that can be instantiated as an element', () => {
         assert(tu.isElement(<HotelsOnMap/>));
+    });
+
+    describe('HTML', () => {
+        let element;
+
+        beforeEach(() => {
+            element = tu.renderIntoDocument(<HotelsOnMap/>).getDOMNode();
+        });
+
+        it('declares the top level CSS class', () => {
+            massert.cssClass(element, 'hotels-on-map');
+        });
     });
 });
