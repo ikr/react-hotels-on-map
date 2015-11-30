7b1854cd5c3efa4c88cd961bf2c878ab60209d86
Ivan Krechetov <ikr@ikr.su>
2015-11-30 09:36:26 +0100

    Mapify before _all_ the test suites

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -6,9 +6,9 @@ import HotelsOnMap from '../src/index';
 
 const tu = React.addons.TestUtils;
 
-describe('HotelsOnMap -- the public API', () => {
-    before(mapify);
+before(mapify);
 
+describe('HotelsOnMap -- the public API', () => {
     it('is a React component that can be instantiated as an element', () => {
         assert(tu.isElement(<HotelsOnMap/>));
     });
