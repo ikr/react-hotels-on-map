14b17a5888ad38e04e369a485182221a68bfe042
Ivan Krechetov <ikr@ikr.su>
2015-11-30 11:18:54 +0100

    Start internalizing the MarkerClusterer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import MarkerClusterer from './MarkerClusterer';
 import defaultMapConfiguration from './defaultMapConfiguration';
+import marker from './marker';
 
 export default React.createClass({
     render() {
@@ -7,9 +9,13 @@ export default React.createClass({
     },
 
     componentDidMount() {
-        /* global global */
         const maps = global.window.google.maps;
+        const map = new maps.Map(this.getDOMNode(), defaultMapConfiguration());
 
-        new maps.Map(this.getDOMNode(), defaultMapConfiguration());
+        new MarkerClusterer(map, this.markers());
+    },
+
+    markers() {
+        return this.props.hotels.map(({geolocation}) => marker(geolocation));
     }
 });
