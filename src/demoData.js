5ec16fc657f8aac6881c6aad56121216966cd868
Ivan Krechetov <ikr@ikr.su>
2015-12-08 17:12:47 +0100

    The new, extended demo data

--- src/demoData.js
+++ src/demoData.js
@@ -1,184 +1,336 @@
-export default [{
-    name: 'Hotel Ambassador à l\'Opéra',
-    geolocation: {latitude: 47.364338, longitude: 8.547041}
-}, {
-    name: 'The Dolder Grand',
-    geolocation: {latitude: 47.37266, longitude: 8.574215}
-}, {
-    name: 'Ramada Hotel Zürich City',
-    geolocation: {latitude: 47.38238, longitude: 8.500547}
-}, {
-    name: 'Hotel Swiss Night am Kunsthaus',
-    geolocation: {latitude: 47.36918, longitude: 8.551184}
-}, {
-    name: 'Hotel Wassberg',
-    geolocation: {latitude: 47.3267, longitude: 8.6536}
-}, {
+export default [{ratingStars: 4,
+    name: '^MGallery Hotel Continental Zürich',
+    streetAddress: 'Stampfenbachstrasse 60',
+    postalCode: '8006',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.3811, longitude: 8.542556}},
+  {ratingStars: 3,
     name: 'Trafo Hotel',
-    geolocation: {latitude: 47.477966, longitude: 8.304228}
-}, {
-    name: 'Sorell Hotel Arte',
-    geolocation: {latitude: 47.41831, longitude: 8.373271}
-}, {
-    name: 'Hotel Park Hyatt Zurich',
-    geolocation: {latitude: 47.36674, longitude: 8.536352}
-}, {
-    name: 'Sorell Hotel Seefeld',
-    geolocation: {latitude: 47.361645, longitude: 8.551362}
-}, {
-    name: 'Hotel Schweizerhof Zürich',
-    geolocation: {latitude: 47.37693, longitude: 8.539398}
-}, {
-    name: 'Hotel Senator',
-    geolocation: {latitude: 47.38939, longitude: 8.522065}
-}, {
+    streetAddress: 'Bruggerstrasse 56',
+    postalCode: '5400',
+    cityLocalized: 'Baden',
+    geolocation: {latitude: 47.477966, longitude: 8.304228}},
+  {ratingStars: 3,
     name: 'Sorell Hotel Rütli',
-    geolocation: {latitude: 47.37615, longitude: 8.544374}
-}, {
-    name: 'Hotel Platzhirsch',
-    geolocation: {latitude: 47.37375, longitude: 8.54397}
-}, {
-    name: 'Hotel Eden au Lac',
-    geolocation: {latitude: 47.36278, longitude: 8.547419}
-}, {
-    name: 'Kameha Grand Zürich',
-    geolocation: {latitude: 47.421684, longitude: 8.560746}
-}, {
-    name: 'Hotel Seedamm Plaza',
-    geolocation: {latitude: 47.203106, longitude: 8.790072}
-}, {
-    name: 'Hotel Meierhof',
-    geolocation: {latitude: 47.26119, longitude: 8.596673}
-}, {
-    name: 'Hotel Dolder Waldhaus',
-    geolocation: {latitude: 47.373108, longitude: 8.569125}
-}, {
-    name: 'Sorell Hotel Zürichberg',
-    geolocation: {latitude: 47.38246, longitude: 8.5669}
-}, {
-    name: 'Hotel Conti Swiss Q',
-    geolocation: {latitude: 47.41261, longitude: 8.402095}
-}, {
-    name: 'Four Points by Sheraton Sihlcity Zürich',
-    geolocation: {latitude: 47.35915, longitude: 8.523142}
-}, {
-    name: 'Hotel Ascot',
-    geolocation: {latitude: 47.36387, longitude: 8.532098}
-}, {
-    name: 'Blue City Hotel',
-    geolocation: {latitude: 47.47792, longitude: 8.306244}
-}, {
-    name: 'Hotel Belvoir Swiss Q',
-    geolocation: {latitude: 47.30599, longitude: 8.55011}
-}, {
-    name: 'Sorell Hotel Sonnental',
-    geolocation: {latitude: 47.396156, longitude: 8.604588}
-}, {
-    name: 'Steigenberger Hotel Bellerive au Lac',
-    geolocation: {latitude: 47.36228, longitude: 8.547396}
-}, {
-    name: 'Hotel Opera',
-    geolocation: {latitude: 47.364285, longitude: 8.547596}
-}, {
+    streetAddress: 'Zähringerstrasse 43',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37615, longitude: 8.544374}},
+  {ratingStars: 3,
+    name: 'Hotel Kindli',
+    streetAddress: 'Pfalzgasse 1',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37239, longitude: 8.540628}},
+  {ratingStars: 5,
     name: 'Baur au Lac',
-    geolocation: {latitude: 47.367302, longitude: 8.539358}
-}, {
-    name: 'Hotel Sedartis Swiss Q',
-    geolocation: {latitude: 47.29553, longitude: 8.565904}
-}, {
-    name: 'Hotel Engimatt',
-    geolocation: {latitude: 47.3599, longitude: 8.524704}
-}, {
+    streetAddress: 'Talstrasse 1',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.367302, longitude: 8.539358}},
+  {ratingStars: 4,
+    name: 'Hotel Ambassador à l\'Opéra',
+    streetAddress: 'Falkenstrasse 6',
+    postalCode: '8008',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.364338, longitude: 8.547041}},
+  {ratingStars: 4,
     name: 'Romantik Seehotel Sonne',
-    geolocation: {latitude: 47.319035, longitude: 8.578894}
-}, {
-    name: 'Radisson Blu Hotel, Zurich-Airport',
-    geolocation: {latitude: 47.45822, longitude: 8.555475}
-}, {
-    name: 'Hotel Zur Therme',
-    geolocation: {latitude: 47.588985, longitude: 8.28793}
-}, {
-    name: 'Mövenpick Hotel Zürich Airport',
-    geolocation: {latitude: 47.439526, longitude: 8.568866}
-}, {
-    name: 'Hotel Fly Away',
-    geolocation: {latitude: 47.45006, longitude: 8.584432}
-}, {
-    name: 'Hilton Zurich Airport',
-    geolocation: {latitude: 47.436897, longitude: 8.571713}
-}, {
-    name: 'Zurich Youth Hostel',
-    geolocation: {latitude: 47.34802, longitude: 8.527802}
-}, {
-    name: 'Hotel Bristol',
-    geolocation: {latitude: 47.37965, longitude: 8.543494}
-}, {
-    name: 'NH Zürich Airport Hotel',
-    geolocation: {latitude: 47.43324, longitude: 8.564604}
-}, {
-    name: 'Storchen Zürich',
-    geolocation: {latitude: 47.37114, longitude: 8.541622}
-}, {
-    name: 'Hotel du Théâtre',
-    geolocation: {latitude: 47.37618, longitude: 8.544732}
-}, {
-    name: 'Hotel Montana',
-    geolocation: {latitude: 47.380814, longitude: 8.53524}
-}, {
-    name: 'Hotel Welcome Inn',
-    geolocation: {latitude: 47.44913, longitude: 8.581508}
-}, {
-    name: 'Hotel Atlantis by Giardino',
-    geolocation: {latitude: 47.36303, longitude: 8.495713}
-}, {
-    name: 'Hotel Florhof',
-    geolocation: {latitude: 47.37231, longitude: 8.548725}
-}, {
-    name: 'Hotel Scheuble',
-    geolocation: {latitude: 47.37446, longitude: 8.544855}
-}, {
-    name: 'Dorint Airport Hotel Zürich',
-    geolocation: {latitude: 47.43679, longitude: 8.567336}
-}, {
-    name: 'Hotel St. Gotthard',
-    geolocation: {latitude: 47.376316, longitude: 8.539158}
-}, {
-    name: 'Hotel Europe',
-    geolocation: {latitude: 47.364185, longitude: 8.547145}
-}, {
-    name: 'Park Hotel Winterthur Swiss Q',
-    geolocation: {latitude: 47.50098, longitude: 8.732851}
-}, {
-    name: 'Sorell Hotel Rex',
-    geolocation: {latitude: 47.384815, longitude: 8.543057}
-}, {
+    streetAddress: 'Seestrasse 120',
+    postalCode: '8700',
+    cityLocalized: 'Zürich-Küsnacht',
+    geolocation: {latitude: 47.319035, longitude: 8.578894}},
+  {ratingStars: 5,
+    name: 'Zürich Marriott Hotel',
+    streetAddress: 'Neumühlequai 42',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.382484, longitude: 8.540407}},
+  {ratingStars: 4,
     name: 'Swissôtel Zürich',
-    geolocation: {latitude: 47.41076, longitude: 8.543684}
-}, {
-    name: 'Hotel Wellenberg',
-    geolocation: {latitude: 47.373016, longitude: 8.543885}
-}, {
+    streetAddress: 'Schulstrasse 44',
+    postalCode: '8050',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.41076, longitude: 8.543684}},
+  {ratingStars: 3,
+    name: 'Sorell Hotel Seefeld',
+    streetAddress: 'Seefeldstrasse 63',
+    postalCode: '8008',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.361645, longitude: 8.551362}},
+  {ratingStars: 5,
+    name: 'Hotel Park Hyatt Zurich',
+    streetAddress: 'Beethoven-Strasse 21',
+    postalCode: '8002',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.36674, longitude: 8.536352}},
+  {ratingStars: 4,
+    name: 'Greulich Design & Lifestyle Hotel',
+    streetAddress: 'Herman-Greulich-Strasse 56',
+    postalCode: '8004',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37847, longitude: 8.520501}},
+  {ratingStars: 4,
+    name: 'Ramada Hotel Zürich City',
+    streetAddress: 'Badenerstrasse 537',
+    postalCode: '8048',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.38238, longitude: 8.500547}},
+  {ratingStars: 3,
+    name: 'Hotel Coronado',
+    streetAddress: 'Schaffhauserstrasse 137',
+    postalCode: '8057',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.39737, longitude: 8.540933}},
+  {ratingStars: 3,
+    name: 'Hotel Swiss Night am Kunsthaus',
+    streetAddress: 'Steinwiesstrasse 8',
+    postalCode: '8032',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.36918, longitude: 8.551184}},
+  {ratingStars: 4,
+    name: 'Hotel Belvoir Swiss Q',
+    streetAddress: 'Saeumerstrasse 37',
+    postalCode: '8803',
+    cityLocalized: 'Rueschlikon',
+    geolocation: {latitude: 47.30599, longitude: 8.55011}},
+  {ratingStars: 4,
+    name: 'Hotel Conti Swiss Q',
+    streetAddress: 'Heimstrasse 41',
+    postalCode: '8953',
+    cityLocalized: 'Dietikon',
+    geolocation: {latitude: 47.41261, longitude: 8.402095}},
+  {ratingStars: 4,
+    name: 'Four Points by Sheraton Sihlcity Zürich',
+    streetAddress: 'Kalandergasse 1',
+    postalCode: '8045',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.359154, longitude: 8.523142}},
+  {ratingStars: 4,
+    name: 'Blue City Hotel',
+    streetAddress: 'Haselstrasse 17',
+    postalCode: '5400',
+    cityLocalized: 'Baden',
+    geolocation: {latitude: 47.47792, longitude: 8.306244}},
+  {ratingStars: 4,
+    name: 'Hotel Ascot',
+    streetAddress: 'Tessinerplatz 9',
+    postalCode: '8002',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.36387, longitude: 8.532098}},
+  {ratingStars: 3,
+    name: 'Apart-Hotel operated by Hilton',
+    streetAddress: 'Hohenbuehlstrasse 1',
+    postalCode: '8152',
+    cityLocalized: 'Opfikon',
+    geolocation: {latitude: 47.43824, longitude: 8.57381}},
+  {ratingStars: 4,
+    name: 'Hotel Opera',
+    streetAddress: 'Dufourstrasse 5',
+    postalCode: '8008',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.364285, longitude: 8.547596}},
+  {ratingStars: 4,
+    name: 'Hotel Engimatt',
+    streetAddress: 'Engimattstrasse 14',
+    postalCode: '8002',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.3599, longitude: 8.524704}},
+  {ratingStars: 4,
+    name: 'Steigenberger Hotel Bellerive au Lac',
+    streetAddress: 'Utoquai 47',
+    postalCode: '8008',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.36228, longitude: 8.547396}},
+  {ratingStars: 4,
+    name: 'Hotel Seedamm Plaza',
+    streetAddress: 'Seedammstrasse 3',
+    postalCode: '8808',
+    cityLocalized: 'Pfäffikon',
+    geolocation: {latitude: 47.203106, longitude: 8.790072}},
+  {ratingStars: 4,
+    name: 'Sorell Hotel Zürichberg',
+    streetAddress: 'Orellistrasse 21',
+    postalCode: '8044',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.38246, longitude: 8.5669}},
+  {ratingStars: 4,
+    name: 'Hotel Meierhof',
+    streetAddress: 'Bahnhofstrasse 4',
+    postalCode: '8810',
+    cityLocalized: 'Zürich-Horgen',
+    geolocation: {latitude: 47.26119, longitude: 8.596673}},
+  {ratingStars: 4,
+    name: 'Hotel Sedartis Swiss Q',
+    streetAddress: 'Bahnhofstrasse 15/16',
+    postalCode: '8800',
+    cityLocalized: 'Thalwil',
+    geolocation: {latitude: 47.29553, longitude: 8.565904}},
+  {ratingStars: 3,
+    name: 'Hotel Fly Away',
+    streetAddress: 'Marktgasse 19',
+    postalCode: '8302',
+    cityLocalized: 'Zürich-Kloten',
+    geolocation: {latitude: 47.45006, longitude: 8.584432}},
+  {ratingStars: 3,
+    name: 'Hotel Zur Therme',
+    streetAddress: 'Quellenstrasse 31',
+    postalCode: '5330',
+    cityLocalized: 'Bad Zurzach',
+    geolocation: {latitude: 47.588985, longitude: 8.28793}},
+  {ratingStars: 4,
+    name: 'Radisson Blu Hotel, Zurich-Airport',
+    streetAddress: '',
+    postalCode: '8058',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.45822, longitude: 8.555475}},
+  {ratingStars: 4,
     name: 'BEST WESTERN PREMIER Hotel Glockenhof',
-    geolocation: {latitude: 47.37288, longitude: 8.536589}
-}, {
-    name: 'Crowne Plaza',
-    geolocation: {latitude: 47.3805, longitude: 8.506285}
-}, {
-    name: 'Walhalla Hotel',
-    geolocation: {latitude: 47.38025, longitude: 8.537229}
-}, {
-    name: 'Park Inn by Radisson Zurich Airport',
-    geolocation: {latitude: 47.45128, longitude: 8.540061}
-}, {
-    name: 'Hotel Adler',
-    geolocation: {latitude: 47.37313, longitude: 8.543516}
-}, {
-    name: 'Sorell Hotel Krone',
-    geolocation: {latitude: 47.49944, longitude: 8.727428}
-}, {
-    name: 'Sorell Hotel Seidenhof',
-    geolocation: {latitude: 47.373688, longitude: 8.537305}
-}, {
+    streetAddress: 'Sihlstrasse 31',
+    postalCode: '8022',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37288, longitude: 8.536589}},
+  {ratingStars: 4,
     name: 'Hotel Allegra',
-    geolocation: {latitude: 47.44872, longitude: 8.584882}
-}];
+    streetAddress: 'Hamelirainstrasse 3',
+    postalCode: '8302',
+    cityLocalized: 'Zürich-Kloten',
+    geolocation: {latitude: 47.44872, longitude: 8.584882}},
+  {ratingStars: 3,
+    name: 'Hotel Bristol',
+    streetAddress: 'Stampfenbachstrasse 34',
+    postalCode: '8006',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37965, longitude: 8.543494}},
+  {ratingStars: 4,
+    name: 'Hilton Zurich Airport',
+    streetAddress: 'Hohenbuehlstrasse 10',
+    postalCode: '8152',
+    cityLocalized: 'Opfikon',
+    geolocation: {latitude: 47.436897, longitude: 8.571713}},
+  {ratingStars: 4,
+    name: 'Storchen Zürich',
+    streetAddress: 'Storchengasse 16',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37114, longitude: 8.541622}},
+  {ratingStars: 3,
+    name: 'Hotel du Théâtre',
+    streetAddress: 'Seilergraben 69',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.376183, longitude: 8.544732}},
+  {ratingStars: 3,
+    name: 'Hotel Montana',
+    streetAddress: 'Konradstrasse 39',
+    postalCode: '8005',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.380814, longitude: 8.53524}},
+  {ratingStars: 3,
+    name: 'Hotel Florhof',
+    streetAddress: 'Florhofgasse 4',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37231, longitude: 8.548725}},
+  {ratingStars: 4,
+    name: 'NH Zürich Airport Hotel',
+    streetAddress: 'Schaffhauserstrasse 101',
+    postalCode: '8152',
+    cityLocalized: 'Zürich-Glattbrugg',
+    geolocation: {latitude: 47.43324, longitude: 8.564604}},
+  {ratingStars: 4,
+    name: 'Mövenpick Hotel Zürich Airport',
+    streetAddress: 'Walter Mittelholzerstrasse 8',
+    postalCode: '8152',
+    cityLocalized: 'Zürich-Glattbrugg',
+    geolocation: {latitude: 47.439526, longitude: 8.568866}},
+  {ratingStars: 5,
+    name: 'Hotel Atlantis by Giardino',
+    streetAddress: 'Döltschiweg 234',
+    postalCode: '8055',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.36303, longitude: 8.495713}},
+  {ratingStars: 3,
+    name: 'Sorell Hotel Rex',
+    streetAddress: 'Weinbergstrasse 92',
+    postalCode: '8006',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.384815, longitude: 8.543057}},
+  {ratingStars: 4,
+    name: 'Park Hotel Winterthur Swiss Q',
+    streetAddress: 'Stadthausstrasse 4',
+    postalCode: '8400',
+    cityLocalized: 'Winterthur',
+    geolocation: {latitude: 47.50098, longitude: 8.732851}},
+  {ratingStars: 4,
+    name: 'Hotel Wellenberg',
+    streetAddress: 'Niederdorfstrasse 10',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.373016, longitude: 8.543885}},
+  {ratingStars: 4,
+    name: 'Hotel Senator',
+    streetAddress: 'Heinrichstrasse 254',
+    postalCode: '8005',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.38939, longitude: 8.522065}},
+  {ratingStars: 0,
+    name: 'Hotel Platzhirsch',
+    streetAddress: 'Spitalgasse 3',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37375, longitude: 8.54397}},
+  {ratingStars: 5,
+    name: 'Hotel Eden au Lac',
+    streetAddress: 'Utoquai 45',
+    postalCode: '8008',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.36278, longitude: 8.547419}},
+  {ratingStars: 4,
+    name: 'Hotel Schweizerhof Zürich',
+    streetAddress: 'Bahnhofplatz 7',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37693, longitude: 8.539398}},
+  {ratingStars: 4,
+    name: 'Crowne Plaza',
+    streetAddress: 'Badenerstrasse 420',
+    postalCode: '8040',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.3805, longitude: 8.506285}},
+  {ratingStars: 3,
+    name: 'Hotel Adler',
+    streetAddress: 'Rosengasse 10',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.373127, longitude: 8.543615}},
+  {ratingStars: 3,
+    name: 'Sorell Hotel Seidenhof',
+    streetAddress: 'Sihlstrasse 9',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.373688, longitude: 8.537305}},
+  {ratingStars: 3,
+    name: 'Hotel Scheuble',
+    streetAddress: 'Mühlegasse 17',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37446, longitude: 8.544855}},
+  {ratingStars: 3,
+    name: 'Leoneck Hotel',
+    streetAddress: 'Leonhardstrasse 1',
+    postalCode: '8001',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.37919, longitude: 8.545258}},
+  {ratingStars: 4,
+    name: 'Hotel St. Gotthard',
+    streetAddress: 'Bahnhofstrasse 87',
+    postalCode: '8021',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.376316, longitude: 8.539158}},
+  {ratingStars: 4,
+    name: 'Hotel Europe',
+    streetAddress: 'Dufourstrasse 4',
+    postalCode: '8008',
+    cityLocalized: 'Zürich',
+    geolocation: {latitude: 47.364185, longitude: 8.547145}}];
